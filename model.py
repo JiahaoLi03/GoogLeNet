d95647c541bd2f94de82f88d86d313e9c1889467
admin <qlyfdljh@163.com>
2025-01-03 22:26:11 +0800

    GoogLeNet模型搭建

--- model.py
+++ model.py
@@ -34,4 +34,78 @@ class Inception(nn.Module):
         return torch.cat((p1, p2, p3, p4), dim=1)
 
 
+class GoogLeNet(nn.Module):
+    def __init__(self, Inception):
+        super(GoogLeNet, self).__init__()
+
+        self.b1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.b2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.b3 = nn.Sequential(
+            Inception(in_channels=192, c1=64, c2=(96, 128), c3=(16, 32), c4=32),
+            Inception(in_channels=256, c1=128, c2=(128, 192), c3=(32, 96), c4=64),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.b4 = nn.Sequential(
+            Inception(in_channels=480, c1=192, c2=(96, 208), c3=(16, 48), c4=64),
+            Inception(in_channels=512, c1=160, c2=(112, 224), c3=(24, 64), c4=64),
+            Inception(in_channels=512, c1=128, c2=(128, 256), c3=(24, 64), c4=64),
+            Inception(in_channels=512, c1=112, c2=(128, 288), c3=(32, 64), c4=64),
+            Inception(in_channels=528, c1=256, c2=(160, 320), c3=(32, 128), c4=128),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.b5 = nn.Sequential(
+            Inception(in_channels=832, c1=256, c2=(160, 320), c3=(32, 128), c4=128),
+            Inception(in_channels=832, c1=384, c2=(192, 384), c3=(48, 128), c4=128),
+            nn.AdaptiveAvgPool2d((1,1)),  # 全局平均池化
+            nn.Flatten(),
+            nn.Linear(in_features=1024, out_features=10),
+        )
+
+        # 权重初始化
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+
+    def forward(self, x):
+        x = self.b1(x)
+        x = self.b2(x)
+        x = self.b3(x)
+        x = self.b4(x)
+        x = self.b5(x)
+
+        return x
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    GoogleNet = GoogLeNet(Inception).to(device)
+
+    print(summary(GoogleNet, (1, 224, 224)))
+
+
+
+
 
